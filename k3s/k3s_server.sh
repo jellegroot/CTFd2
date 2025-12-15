74c274d0f392fce61a04b23055ecd1cd40b7baca
jellegroot <jelle.groot@hotmail.nl>
2025-12-15 12:17:42 +0000

    server script works k3s

--- k3s/k3s_server.sh
+++ k3s/k3s_server.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -e
 
 # ===== CONFIG =====
@@ -8,18 +7,16 @@ DB_USER="cybermeister"
 DB_PASS="cyber123"
 DB_PORT="33306"
 K3S_VERSION=""
-LOADBALANCER_IP="192.168.2.24"
+LOADBALANCER_IP="192.168.2.26"
 # ==================
 
 echo "==> Update system"
 sudo apt update
 
-#====== Let no Pods run on server =========
-curl -sfL https://get.k3s.io sh -s - server --node-taint CriticalAddonsOnly=true:NoExecute --tls-san ${LOADBALANCER_IP}
+echo "==> Install k3s server (single run)"
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION="${K3S_VERSION}" sh -s - server \
+  --datastore-endpoint="mysql://${DB_USER}:${DB_PASS}@tcp(127.0.0.1:${DB_PORT})/${DB_NAME}" \
+  --node-taint CriticalAddonsOnly=true:NoExecute \
+  --tls-san ${LOADBALANCER_IP}
 
-
-
-
-
-curl -sfL https://get.k3s.io |  INSTALL_K3S_VERSION=${K3S_VERSION} sh -s - server \
-  --datastore-endpoint="mysql://${DB_USER}:${DB_PASS}@localhost:${DB_PORT}/${DB_NAME}"
+echo "==> DONE"
