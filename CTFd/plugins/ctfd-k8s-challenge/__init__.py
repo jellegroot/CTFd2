a7ca4d21ae7d4ff493ebbd2476f00de1eb2bac34
Jelle Groot <jelle.groot@hotmail.nl>
2026-01-05 21:31:48 +0100

    removed k8s

--- CTFd/plugins/ctfd-k8s-challenge/__init__.py
+++
@@ -1,7 +0,0 @@
-def load(app):
-    """
-    Minimal k8s challenge plugin loader.
-    This is a placeholder that allows CTFd to start without errors.
-    Actual k8s challenge implementation should be added here.
-    """
-    pass
